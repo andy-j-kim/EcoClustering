2cf09c680433807fa4d3a029db9e710b37ea80dc
andy-j-kim <a_kim@berkeley.edu>
2024-07-11 17:07:24 +0200

    added summaries, new files for analysis

---
+++ EcoClustering_Project/clustering_scripts/clustering_ET19.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+####################
+# SBATCH OPTIONS
+####################
+#SBATCH --job-name=EcoClustering_ET19
+#SBATCH --account=co_biostat
+#SBATCH --partition=savio3
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=32
+#SBATCH --time=60:00:00
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=a_kim@berkeley.edu
+
+####################
+# What to run (replace cc with correct country code [above too])
+####################
+
+module load r
+module load r-packages
+module load r-spatial
+R CMD BATCH --no-save ../EcoClustering/R/clustering_ET19.R ../EcoClustering/R_output/clustering_ET19.Rout
